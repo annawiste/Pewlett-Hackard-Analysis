1651e18d24d1e2d3d69e4fc3e2679f257b53094f
annawiste <12013474+annawiste@users.noreply.github.com>
2021-10-18 11:24:28 -0400

    fixed table schema

--- schema.sql
+++ schema.sql
@@ -18,12 +18,12 @@ CREATE TABLE employees (
 
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+emp_no INT NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -40,14 +40,14 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL, 
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_employees (
 	emp_no INT NOT NULL, 
 	dept_no VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
